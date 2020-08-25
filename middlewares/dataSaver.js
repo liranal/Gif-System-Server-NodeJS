18ea040f2278a5e0e6e26a4c8830dc9b79c03c23
liranal <liraneilary@gmail.com>
2020-08-26 01:15:43 +0300

    cleaning dataSaver

--- middlewares/dataSaver.js
+++ middlewares/dataSaver.js
@@ -3,7 +3,9 @@ const postMethodMiddleWare = require("./postMiddleware");
 const patchMethodMiddleWare = require("./patchMiddleware");
 const deleteMethodMiddleWare = require("./deleteMiddleware");
 
-exports.getMethodMiddleWare = getMethodMiddleWare;
-exports.postMethodMiddleWare = postMethodMiddleWare;
-exports.patchMethodMiddleWare = patchMethodMiddleWare;
-exports.deleteMethodMiddleWare = deleteMethodMiddleWare;
+module.exports = {
+  getMethodMiddleWare,
+  postMethodMiddleWare,
+  patchMethodMiddleWare,
+  deleteMethodMiddleWare,
+};
