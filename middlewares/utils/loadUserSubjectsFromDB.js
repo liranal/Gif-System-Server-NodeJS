513f2d223e0526c018d2a5031aff05eb72b98fdb
liranal <liraneilary@gmail.com>
2020-08-26 12:25:47 +0300

    mongoCloud new connection

--- middlewares/utils/loadUserSubjectsFromDB.js
+++ middlewares/utils/loadUserSubjectsFromDB.js
@@ -1,7 +1,6 @@
 const UserSubject = require("../../models/UserSubject");
 const UsersSubjects = require("../../GlobalObjects/UsersSubjects");
 module.exports = (userId) => {
-  console.log("LOADING");
   UserSubject.find({ userId: userId }, function (err, userSubjects) {
     console.log("FINDING");
     if (!err) UsersSubjects.set(userId, [...userSubjects]);
