414b35ce224228cba3942f9014adc7fc54aaf292
liranal <liraneilary@gmail.com>
2020-08-26 12:00:59 +0300

    mongoCloud mongoclient

--- config/database.js
+++ config/database.js
@@ -1,12 +1,24 @@
 const mongoose = require("mongoose");
 
 //mongoose.connect("mongodb://localhost:27017/gifSystemDB");
-
+/*
 mongoose.connect(
   "mongodb+srv://liranal:liraneilary1@gif-system.wslz3.mongodb.net/gifSystemDB?retryWrites=true&w=majority"
 );
-mongoose.set("debug", true);
+
 const db = mongoose.connection;
 
 db.once("open", () => console.log("Database opened..."));
 db.on("error", () => console.log("Error occured.."));
+
+*/
+const MongoClient = require("mongodb").MongoClient;
+const uri =
+  "mongodb+srv://liranal:liraneilary1@gif-system.wslz3.mongodb.net/gifSystemDB?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect((err) => {
+  client.close();
+});
+
+client.once("open", () => console.log("Database opened..."));
+client.on("error", () => console.log("Error occured.."));
